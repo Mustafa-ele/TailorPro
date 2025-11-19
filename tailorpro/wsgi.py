4b61b5d4e265d0ea05345ae503b4462050d3b9a7
Mustafa-ele <royalenemy13@gmail.com>
2025-11-19 12:14:36 +0530

    Update wsgi.py

--- tailorpro/wsgi.py
+++ tailorpro/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for tailorpro project.
 
@@ -9,27 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tailorpro.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for tailorpro project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tailorpro.settings')
-
-application = get_wsgi_application()
->>>>>>> 25ebfac29b52da4c94d0490e2f97baf08cb66efc
